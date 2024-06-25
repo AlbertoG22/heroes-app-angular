ea8b75eec014ebabc263f8697b8b78e5f1105c00
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-06-24 20:54:55 -0600

    input autocomplete pt2

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search-page',
@@ -7,5 +9,17 @@ import { FormControl } from '@angular/forms';
   styles: ``
 })
 export class SearchPageComponent {
+
   public searchInput = new FormControl();
+  public heroes: Hero[] = [];
+
+  constructor( private heroesService: HeroesService ) {}
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+    
+    this.heroesService.getSuggestions( value )
+      .subscribe( heroes => this.heroes = heroes );
+  }
+
 }
