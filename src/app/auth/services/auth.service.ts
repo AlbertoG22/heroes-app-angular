b93872d8e614fdec837cc170d1c6fbb3350d3c5a
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-02 20:51:03 -0600

    authenticate and show active user

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -24,8 +24,13 @@ export class AuthService {
     return this.http.get<User>(`${ this.baseUrl }/users/1`)
       .pipe(
         tap( user => this.user = user ),
-        tap( user => localStorage.setItem( 'token', user.id.toString() ))
+        tap( user => localStorage.setItem( 'token', 'aNh8BGuy7aI8Tr2w' ))
       )
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
   
 }
