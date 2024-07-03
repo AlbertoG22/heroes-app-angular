e835cb10795985ef7da666d86509893b5233ba1a
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-02 21:14:07 -0600

    maintain user session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'heroesApp';
+
+  /*
+    aquí no debe aplicarse este checkAuthentication(), ya que primero carga la data y después revisa la 
+    sesión, cargando primero info que posiblemente no debería ver el usuario activo
+  */
+  
+  // constructor( private authService: AuthService ) { }
+  
+  // ngOnInit(): void {
+  //   this.authService.checkAuthentication().subscribe( () => {
+  //     console.log('checkAuthentication finished');
+  //   })
+  // }
 }
